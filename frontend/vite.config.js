94bba839ee15c8f1e57daa396a3dabeb2696091e
JotysRafly <jotys.rafly@student.umn.ac.id>
2024-10-25 15:06:41 +0700

    fix: Docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Jotys/',  // Ganti 'Nama' dengan subdirektori yang kamu inginkan
   plugins: [vue()],
-})
+  server: {
+    host: '0.0.0.0',  // Mengekspos server ke jaringan publik
+    port: 5173,       // Port default untuk Vite
+  },
+});
